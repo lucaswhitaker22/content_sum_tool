4e13a318f6500de90944640b8ffd883fb161f24d
lwhitaker <lwhitaker@rbbn.com>
2024-09-20 14:58:54 -0400

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# content_sum_tool
---
+++ src/models/lecture.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const lectureSchema = new mongoose.Schema({
+  metadata: {
+    overview: String,
+    topics: [String],
+    format: String,
+    date: Date,
+    course: String,
+    title: String,
+    path: String
+  },
+  notes: String,
+  review: [{
+    question: String,
+    answer: String
+  }],
+  keywords: [{
+    term: String,
+    definition: String
+  }],
+  practice: {
+    long: [{
+      question: String,
+      answer: String
+    }],
+    multiple: [{
+      question: String,
+      options: [String],
+      answer: String,
+      explanation: String
+    }],
+    short: [{
+      question: String,
+      answer: String
+    }]
+    }
+});
+
+module.exports = mongoose.model('Lecture', lectureSchema);
